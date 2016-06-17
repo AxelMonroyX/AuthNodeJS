65f2716a1c8ac48d8e772f9bf29a2bca26e7cfca
Axel Monroy <xaxelmonroyx@gmail.com>
2016-06-16 18:10:13 -0700

    Created methods to get and create user with the route POST	 /api/user	 Add a single user GET	 /api/user	 Get Users without authentication

--- app/controllers/authCtrl.js
+++ app/controllers/authCtrl.js
@@ -11,8 +11,47 @@ router.get('/login', function (req, res, next) {
     title: 'Log in',
   });
 });
+
 router.get('/signup', function (req, res, next) {
   res.render('auth/signup', {
     title: 'signup',
   });
+});
+
+router.post('/', function (req, res, next) {
+  User.findOne({
+    email: req.body.email
+  }, function (err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      });
+    } else if (user) {
+
+      if (user.password != req.body.password) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password.'
+        });
+      } else {
+
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+
 });
--- app/controllers/userCtrl.js
+++ app/controllers/userCtrl.js
@@ -1,24 +1,60 @@
 var express = require('express'),
   router = express.Router(),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
+
+
 
 module.exports = function (app) {
-  app.use('/user', router);
+  app.use('/api/user', router);
 };
+
+router.get('/', function (req, res, next) {
+  User.find(function (err, users) {
+    if (err) return res.send(500, err.message);
+    res.status(200).jsonp(users);
+  });
+
+
+
+});
 
 router.get('/:user_id', function (req, res, next) {
 
   res.status(500).json({
-    error: 'User with the ID '+req.params.user_id+' not found'
+    error: 'User with the ID ' + req.params.user_id + ' not found'
   });
 });
 
 router.post('/', function (req, res, next) {
-  res.json(null);
-  res.json({
-    user: 'tobi'
-  });
-  res.status(500).json({
-    error: 'message'
-  });
+
+  if (req.body.password === req.body.password_confirmed) {
+    var newUser = new User({
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+      role: 1
+    });
+    console.log("not saved");
+    newUser.save(function (err, userregistered) {
+      if (err) return res.send(500, err.message);
+        res.status(200).json({
+          success: true
+        }, userregistered);
+      }
+
+    );
+    console.log("saved");
+
+
+
+
+
+  } else {
+    res.json({
+      success: false,
+      error: 'Confirme your password please'
+    });
+  }
+
 });
